7e7f6046f42682f1899aff4196821648988ce580
HELEN <alenagolosova97@gmail.com>
2026-04-17 16:06:43 +0300

    gallery,form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,35 @@
-console.log('form');
+const form = document.querySelector('.feedback-form');
+const formData = {
+  email: '',
+  message: '',
+};
+form.addEventListener('input', handlerInput);
+function handlerInput(event) {
+  const { name, value } = event.target;
+
+  formData[name] = value.trim();
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+const local = localStorage.getItem('feedback-form-state');
+
+if (local) {
+  const parse = JSON.parse(local);
+  formData.email = parse.email || '';
+  formData.message = parse.message || '';
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+form.addEventListener('submit', handlerBtn);
+function handlerBtn(event) {
+  event.preventDefault();
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  formData.email = '';
+  formData.message = '';
+  form.reset();
+}
